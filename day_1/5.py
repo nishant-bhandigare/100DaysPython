ea4bbec963ce15d2c0ec822ccd5b6f8a265a7f19
nishant-bhandigare <nishantbhandigare@gmail.com>
2023-12-11 08:19:29 +0530

    completed day 1

---
+++ day_1/5.py
@@ -0,0 +1,6 @@
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Don't put anything inside the input() function!
+
+name = input()
+print(len(name))
---
+++ day_1/day_1_project.py
@@ -0,0 +1,15 @@
+#Day 1 Project: Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city that you grew in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
+
+# Output:
+
+# Welcome to the Band Name Generator.
+# What's the name of the city that you grew in?
+# Bristol
+# What's your pet's name?
+# Rabbit
+# Your band name could be Bristol Rabbit
